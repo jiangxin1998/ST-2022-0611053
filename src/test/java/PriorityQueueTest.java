37b1c6ea0faba6038aae3b4b1a86509301798132
Brian Jiang <49828279+jiangxin1998@users.noreply.github.com>
2022-03-12 16:10:52 +0800

    Update PriorityQueueTest.java

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
             return Stream.of(
-                    Arguments.of(new int[]{6,4,3,2,1},new int[]{1,2,3,4,5}),
+                    Arguments.of(new int[]{5,4,3,2,1},new int[]{1,2,3,4,5}),
                     Arguments.of(new int[]{0,-4,9,-2,1},new int[]{-4,-2,0,1,9}),
                     Arguments.of(new int[]{2,-3,4,7,1},new int[]{-3,1,2,4,7}),
                     Arguments.of(new int[]{1,2,-3,-7,0},new int[]{-7,-3,0,1,2}),
